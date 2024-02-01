1f9a915f6e6c5f3ea91404826fa7b53b54495e4b
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-02-01 12:35:59 +0530

    set updatable fields required

--- DTOs/UpdateCustomerRequest.cs
+++ DTOs/UpdateCustomerRequest.cs
@@ -1,43 +1,60 @@
-﻿namespace CodeBuddies_PizzaAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeBuddies_PizzaAPI.DTOs
 {
 	public class UpdateCustomerRequest
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
 		public string Email { get; set; }
+		[Required]
 		public string FirstName { get; set; }
+		[Required]
 		public string LastName { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
+		[Required]
+		public string Address { get; set; }
+		[Required]
+		public string Phone { get; set; }
 	}
 
 	public class UpdateCustomerEmailRequest
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
 		public string Email { get; set; }
 	}
 	public class UpdateCustomerFirstNameRequest
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
 		public string FirstName { get; set; }
 
 	}
 
 	public class UpdateCustomerLastLastRequest
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
 		public string LastName { get; set; }
 	}
 
 	public class UpdateCustomerPhoneRequest
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
 		public string Phone { get; set; }
 	}
 
 	public class UpdateCustomerAddressRequest
 	{
+		[Required]
 		public int Id { get; set; }
-
+		[Required]
 		public string Address { get; set; }
 	}
 }
